51a2f89b50366fd83e65c9a12fcd5de86ec65a5e
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-12-06 16:45:38 +0100

    Add solution to task 0

---
+++ utils/redis.js
@@ -0,0 +1,39 @@
+import { createClient } from "redis";
+import { promisify } from "util";
+
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.isClientConnected = true;
+
+    this.client.on("error", (error) => {
+      console.error("Redis client not connected to the server:", error.message);
+      this.isClientConnected = false;
+    });
+
+    this.client.on("connect", () => {
+      this.isClientConnected = true;
+      console.log("Redis client connected to the server");
+    });
+  }
+
+  isAlive() {
+    return this.isClientConnected;
+  }
+
+  async get(key) {
+    return promisify(this.client.get).bind(this.client)(key);
+  }
+
+  async set(key, value, duration) {
+    await promisify(this.client.setex).bind(this.client)(key, duration, value);
+  }
+
+  async del(key) {
+    await promisify(this.client.del).bind(this.client)(key);
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
